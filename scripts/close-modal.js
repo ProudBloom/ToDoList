0d1bb7ca2f8f9f4e2dbffc0366b72cad10cd48cb
ProudBloom <jakub-sztompka@wp.pl>
2020-10-18 12:41:34 +0200

    Modal menu for tasks addition added

---
+++ scripts/close-modal.js
@@ -0,0 +1,7 @@
+document.querySelector('.add-task-btn').addEventListener('click', function(){
+    document.querySelector('.bg-modal').style.display = 'flex';
+})
+
+document.querySelector('.close-modal').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+})
