9e257e70e06124b0442c3cd0e6596eb95f356eb6
ProudBloom <jakub-sztompka@wp.pl>
2020-10-18 21:08:30 +0200

    Local array replaced by the proper database (in cloud)

--- controllers/todo-cotroller.js
+++ controllers/todo-cotroller.js
@@ -1,26 +1,40 @@
 const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
+const mongoose = require('mongoose');
 
-var data = [];
-var urlEncodedParser = bodyParser.urlencoded({extended: false});
+mongoose.connect('mongodb+srv://test:test@todo.bi8uk.mongodb.net/<dbname>?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true});
+
+//Schema for a DB collection
+var todoSchema = new mongoose.Schema({
+    taskName: String,
+    taskPriority: String
+});
+//Model of data inside the collection
+var Todo = mongoose.model('Todo', todoSchema);
 
 module.exports = function(app)
 {
     app.get('/todolist', function(req, res)
     {
-        res.render('todolist', {todos: data});
+        Todo.find({}, function(err, data){
+            if(err) throw err;
+            res.render('todolist', {todos: data});
+        }); //Get all te items from collection and pass to the view
     });
 
     app.post('/todolist', urlEncodedParser, function(req, res)
     {
-        data.push(req.body);
-        res.json({todos: data});
+        var newTodo = Todo(req.body).save(function(err, data) {
+            if(err) throw err;
+            res.json({todos: data});
+        });
     });
 
     app.delete('/todolist/:taskName', function(req, res)
     {
-        data = data.filter(function(todo){
-            return todo.taskName.replace(/ /g, '-') !== req.params.taskName;
+        Todo.find({taskName: req.params.taskName.replace(/\-/g, ' ')}).deleteOne(function(err, data){
+            if(err) throw err;
+            res.json({todos: data});
         });
-        res.json({todos: data})
     });
 }
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const todoController = require('./controllers/todo-cotroller');
+const app = express();
 const port = 8000;
 
-const app = express();
 app.set('view engine', 'ejs');  //template engine
 app.use(express.static(__dirname + '/assets')); //static files handler (css, js)
 
 todoController(app);
-
 
 app.listen(port);
 console.log('listening on port: ' + port);
